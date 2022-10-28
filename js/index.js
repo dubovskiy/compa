6b9ea997df64b23ef869d912c75a6bf559450bd2
dubovskiy <aleksandr.dubovskiy@gmail.com>
2022-10-28 13:41:29 +0300

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,26 @@
+import cities from '../data/cities.json' assert {type: 'json'};
+
+const l = document.querySelector('.submitWrapper');
+
+navigator.permissions.query({ name:' geolocation' }).then((result) => {
+    if (result.state === 'granted') {
+        l.innerHtml = 'ok';
+    }
+});
+
+
+// const acl = new Accelerometer({ frequency: 60 });
+// acl.addEventListener("reading", () => {
+//     console.log(`Acceleration along the X-axis ${acl.x}`);
+//     console.log(`Acceleration along the Y-axis ${acl.y}`);
+//     console.log(`Acceleration along the Z-axis ${acl.z}`);
+// });
+//
+// acl.start();
+
+let magSensor = new Magnetometer({frequency: 60});
+
+magSensor.addEventListener('reading', (e) => {
+    l.innerHTML = `Magnetic field along the X-axis ${magSensor.x} Magnetic field along the Y-axis ${magSensor.y} Magnetic field along the Z-axis ${magSensor.z}`);
+});
+magSensor.start();
