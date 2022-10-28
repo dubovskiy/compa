e3d113ebe4029abe68e98fe44ebea7b1a1df87ae
Aleksandr Dubovskiy <aleksandr.dubovskiy@gmail.com>
2022-10-28 15:17:04 +0300

    211111

--- js/index.js
+++ js/index.js
@@ -1,63 +1,50 @@
 import cities from '../data/cities.json' assert {type: 'json'};
-let active = null;
-const buttons = document.querySelectorAll('.buttonsWrapper button');
-
-
-
-const acl = new Accelerometer({ frequency: 60 });
-acl.addEventListener("reading", () => {
-    console.log(`Acceleration along the X-axis ${acl.x}`);
-    console.log(`Acceleration along the Y-axis ${acl.y}`);
-    console.log(`Acceleration along the Z-axis ${acl.z}`);
-});
-
-
-
-
-// const options = { frequency: 60, referenceFrame: "device" };
-// const sensor = new AbsoluteOrientationSensor(options);
+// let active = null;
+// const buttons = document.querySelectorAll('.buttonsWrapper button');
 //
-// sensor.addEventListener("reading", () => {
-//     // model is a Three.js object instantiated elsewhere.
-//     console.log(sensor);
+//
+//
+// const acl = new Accelerometer({ frequency: 60 });
+// acl.addEventListener("reading", () => {
+//     console.log(`Acceleration along the X-axis ${acl.x}`);
+//     console.log(`Acceleration along the Y-axis ${acl.y}`);
+//     console.log(`Acceleration along the Z-axis ${acl.z}`);
 // });
-
-start()
-
-
-Promise.all([
-    navigator.permissions.query({ name: "accelerometer" }),
-    navigator.permissions.query({ name: "magnetometer" }),
-    navigator.permissions.query({ name: "gyroscope" }),
-]).then((results) => {
-    if (results.every((result) => result.state === "granted")) {
-
-        function start () {
-            console.log('start')
-        }
-
-        buttons.forEach((button) => {
-            button.addEventListener('click', function (e) {
-                const activeItem = document.querySelector('.buttonsWrapper .active');
-                if (activeItem === this) {
-                    active = '';
-                    this.classList.remove('active');
-                    acl.stop();
-                } else {
-                    active = this.innerHTML;
-                    this.classList.add('active');
-                    activeItem.target.classList.remove('active');
-                    acl.start();
-                }
-            })
-        })
-
-
-
-    } else {
-        console.log("No permissions to use AbsoluteOrientationSensor.");
-    }
-});
+//
+//
+// Promise.all([
+//     navigator.permissions.query({ name: "accelerometer" }),
+//     navigator.permissions.query({ name: "magnetometer" }),
+//     navigator.permissions.query({ name: "gyroscope" }),
+// ]).then((results) => {
+//     if (results.every((result) => result.state === "granted")) {
+//
+//         function start () {
+//             console.log('start')
+//         }
+//
+//         buttons.forEach((button) => {
+//             button.addEventListener('click', function (e) {
+//                 const activeItem = document.querySelector('.buttonsWrapper .active');
+//                 if (activeItem === this) {
+//                     active = '';
+//                     this.classList.remove('active');
+//                     acl.stop();
+//                 } else {
+//                     active = this.innerHTML;
+//                     this.classList.add('active');
+//                     activeItem.target.classList.remove('active');
+//                     acl.start();
+//                 }
+//             })
+//         })
+//
+//
+//
+//     } else {
+//         console.log("No permissions to use AbsoluteOrientationSensor.");
+//     }
+// });
 
 
 // const acl = new Accelerometer({ frequency: 60 });
