603853f34487ee083fe17001ddba0ab62c137eeb
Aleksandr Dubovskiy <aleksandr.dubovskiy@gmail.com>
2022-10-28 15:09:56 +0300

    211111

--- js/index.js
+++ js/index.js
@@ -1,14 +1,29 @@
 import cities from '../data/cities.json' assert {type: 'json'};
+let active = null;
+const buttons = document.querySelectorAll('.buttonsWrapper button');
 
-const l = document.querySelector('.submitWrapper');
 
-const options = { frequency: 60, referenceFrame: "device" };
-const sensor = new AbsoluteOrientationSensor(options);
 
-sensor.addEventListener("reading", () => {
-    // model is a Three.js object instantiated elsewhere.
-    console.log(sensor);
+const acl = new Accelerometer({ frequency: 60 });
+acl.addEventListener("reading", () => {
+    console.log(`Acceleration along the X-axis ${acl.x}`);
+    console.log(`Acceleration along the Y-axis ${acl.y}`);
+    console.log(`Acceleration along the Z-axis ${acl.z}`);
 });
+
+
+
+
+// const options = { frequency: 60, referenceFrame: "device" };
+// const sensor = new AbsoluteOrientationSensor(options);
+//
+// sensor.addEventListener("reading", () => {
+//     // model is a Three.js object instantiated elsewhere.
+//     console.log(sensor);
+// });
+
+start()
+
 
 Promise.all([
     navigator.permissions.query({ name: "accelerometer" }),
@@ -16,7 +31,29 @@ Promise.all([
     navigator.permissions.query({ name: "gyroscope" }),
 ]).then((results) => {
     if (results.every((result) => result.state === "granted")) {
-        sensor.start();
+
+        function start () {
+            console.log('start')
+        }
+
+        buttons.forEach((button) => {
+            button.addEventListener('click', function (e) {
+                const activeItem = document.querySelector('.buttonsWrapper .active');
+                if (activeItem === this) {
+                    active = '';
+                    this.classList.remove('active');
+                    acl.stop();
+                } else {
+                    active = this.innerHTML;
+                    this.classList.add('active');
+                    activeItem.target.classList.remove('active');
+                    acl.start();
+                }
+            })
+        })
+
+
+
     } else {
         console.log("No permissions to use AbsoluteOrientationSensor.");
     }
