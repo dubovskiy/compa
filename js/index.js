94a5ae22afc1a1e704f8b2fbdda702b86b83aad1
Aleksandr Dubovskiy <aleksandr.dubovskiy@gmail.com>
2022-10-28 18:28:13 +0300

    211111

--- js/index.js
+++ js/index.js
@@ -1,30 +1,50 @@
 import cities from '../data/cities.json' assert {type: 'json'};
-window.cities = cities
+
 let active = null;
 const buttons = document.querySelectorAll('.buttonsWrapper button');
 const submit = document.querySelector('.submitWrapper button');
 const compass = document.querySelector('.compass img');
-const options = { frequency: 30, referenceFrame: "device" };
-const location = {
-    longitude: 0,
-    latitude: 0,
-}
-function locationHandler(pos) {
-    location.longitude = pos.coords.longitude;
-    location.latitude = pos.coords.latitude;
+const options = {frequency: 30, referenceFrame: "device"};
+buttons.forEach((button) => {
+    button.addEventListener('click', function (e) {
+        const activeItem = document.querySelector('.buttonsWrapper .active');
+        if (activeItem === this) {
+            active = '';
+            this.classList.remove('active');
+
+        } else {
+            active = this.innerHTML;
+            this.classList.add('active');
+            activeItem?.classList.remove('active');
+        }
+    })
+});
+const currentLocation = {
+    longitude: 35,
+    latitude: 48.45,
 }
 
+
+function locationHandler(pos) {
+    currentLocation.longitude = pos.coords.longitude;
+    currentLocation.latitude = pos.coords.latitude;
+}
+
+function convertLonLatToKm (lon, lat) {
+    const cityLon = lon * 111.1;
+    const oneDegLat = Math.cos(Math.PI * lon / 180) * 111.3;
+    const cityLat = lat * oneDegLat;
+    return [cityLon, cityLat];
+}
 
 Promise.all([
-    navigator.permissions.query({ name: "accelerometer" }),
-    navigator.permissions.query({ name: "magnetometer" }),
-    navigator.permissions.query({ name: "gyroscope" }),
-    navigator.permissions.query({name:"geolocation"}),
+    navigator.permissions.query({name: "accelerometer"}),
+    navigator.permissions.query({name: "magnetometer"}),
+    navigator.permissions.query({name: "gyroscope"}),
 ]).then((results) => {
     if (results.every((result) => result.state === "granted")) {
         let angle;
         navigator.geolocation.getCurrentPosition(locationHandler);
-
 
         const sensor = new AbsoluteOrientationSensor(options);
 
@@ -34,55 +54,40 @@ Promise.all([
         });
         sensor.start();
 
-        buttons.forEach((button) => {
-            button.addEventListener('click', function (e) {
-                const activeItem = document.querySelector('.buttonsWrapper .active');
-                if (activeItem === this) {
-                    active = '';
-                    this.classList.remove('active');
-
-                } else {
-                    active = this.innerHTML;
-                    this.classList.add('active');
-                    activeItem?.classList.remove('active');
-                }
-            })
-        });
-
         submit.addEventListener('click', () => {
+            const [myLon, myLat] = convertLonLatToKm(currentLocation.longitude, currentLocation.latitude);
             const citiesAndDistances = cities.map((city) => {
-                const [lon,lat] = city.geometry.coordinates;
+                const [lon, lat] = city.geometry.coordinates;
+                const [cityLon, cityLat] = convertLonLatToKm(lon, lat);
+
                 const name = city.properties.capital + '-' + city.properties.country;
-                const longDelta = lon - location.longitude;
-                const latDelta = lat - location.latitude;
+                const longDelta = cityLon - myLon;
+                const latDelta = cityLat - myLat;
                 const distToCity = Math.sqrt(longDelta * longDelta + latDelta * latDelta);
-                const angleToCity = Math.atan(longDelta / latDelta);
+                const angleToCity = Math.atan(longDelta / -latDelta);
                 const angleRocket = angle * Math.PI;
-                const dist = sin(angleRocket - angleToCity) * distToCity;
+                const dist = Math.abs(Math.sin(angleRocket - angleToCity) * distToCity);
+                const isMineCity = distToCity < 100;
                 return {
-                   name,
-                   dist,
+                    isMineCity,
+                    name,
+                    distToCity,
+                    angleToCity,
+                    dist,
                 }
             });
-
-            let nearest = citiesAndDistances[0];
-            for (item of citiesAndDistances) {
-                if (nearest.dist > item.dist) {
-                    nearest = item
-                }
-            }
+            citiesAndDistances.sort((item1, item2) => {
+                return item1.dist - item2.dist;
+            })
 
             const data = {
-                'current position': `long: ${lon}, lat: ${lat}`,
+                'current position': `long: ${currentLocation.longitude}, lat: ${currentLocation.latitude}`,
                 'direction angle': `${angle}deg`,
-                'potential target': `${nearest.name}`,
+                'potential target': `${citiesAndDistances[0].isMineCity ? citiesAndDistances[1].name: citiesAndDistances[0].name}`,
             }
             alert(JSON.stringify(data));
         })
-
-
-
     } else {
-        console.log("No permissions to use AbsoluteOrientationSensor.");
+        console.log("No permissions");
     }
 });
